ae1d3d9d01b582ff4cc4a1a04ad09a91cf9e107f
ariiesaka <judahutauruk12@gmail.com>
2024-03-25 01:21:32 +0700

    Implement better detail page

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import Container from "react-bootstrap/Container";
-import { MapContainer, GeoJSON, TileLayer, useMap} from 'react-leaflet';
+import { Container, Card } from "react-bootstrap";
+import { MapContainer, GeoJSON, TileLayer, useMap } from 'react-leaflet';
 import { useParams } from "react-router-dom";
-import Table from 'react-bootstrap/Table';
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from 'react-router-dom';
 
 function ChangeMapView({ bounds }) {
     const map = useMap();
     useEffect(() => {
         if (bounds) {
-            console.log(bounds);
             map.fitBounds(bounds);
         }
     }, [bounds]);
@@ -21,19 +20,19 @@ function ChangeMapView({ bounds }) {
 function Detail() {
     const { iri_peristiwa } = useParams();
     const [response, setResponse] = useState(null);
-    
+
     useEffect(() => {
         let url = 'http://127.0.0.1:8000/map/detail/' + iri_peristiwa;
-        console.log(url)
+        // console.log(url)
         fetch(url, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
         })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => setResponse(data))
-        .catch((error) => console.error(error))
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => { setResponse(data) })
+            .catch((error) => console.error(error));
     }, []);
 
     const maxBounds = [
@@ -43,48 +42,66 @@ function Detail() {
 
     return (
         <Container fluid>
-            {response?.geojson != null && 
-            <MapContainer
-                style={{ height: "40vh"}} 
-                maxBounds={maxBounds}
-                className='rounded'
-            >
-                <TileLayer 
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
-                <GeoJSON
-                    data={response?.geojson}
-                />
-                <ChangeMapView bounds={response?.bounds} />
-            </MapContainer>}
+            {response?.location != null &&
+                <MapContainer
+                    style={{ height: "40vh" }}
+                    maxBounds={maxBounds}
+                    minZoom={3}
+                    className='rounded my-3'
+                >
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <GeoJSON
+                        data={response?.location}
+                    />
+                    <ChangeMapView bounds={response?.bounds} />
+                </MapContainer>}
 
             <div className='p-5 text-center'>
-                <h1 className="my-3" style={{ textAlign: "center" }}>Detail {response?.name}</h1>
+                <h1 style={{ textAlign: "center" }}>Detail {response?.detail.name[1]}</h1>
             </div>
 
-            {response?.detail != null &&
-            <Table striped bordered hover variant="dark">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Properties</th>
-                        <th>Pihak 1</th>
-                        <th>Pihak 2</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.entries(response?.detail).map(([key, value], index) => (
-                        <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{key}</td>
-                            <td>{value}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-            }
-            </Container>
+            {response?.detail ? (
+                <Card className="my-3">
+                    <Card.Header as="h5">Detail Informasi</Card.Header>
+                    <Card.Body>
+                        {Object.entries(response?.detail).map(([key, value]) => {
+                            if (Array.isArray(value) && value.length === 2 && Array.isArray(value[1])) {
+                                return (
+                                    <div key={key} className="mb-3">
+                                        <strong>{value[0]}:</strong>
+                                        {value[1].map((item, index) => (
+                                            <>
+                                                <Link key={index} to={`/detail/${item[0]}`} className="text-primary"> {item[1]}</Link>
+                                                {index < value[1].length - 1 && <>, </>}
+                                            </>
+                                        ))}
+                                    </div>
+                                );
+                            }
+                            else if (Array.isArray(value) && value.length === 2 && value[1] != null) {
+                                return (
+                                    <div key={key} className="mb-3">
+                                        <strong>{value[0]}:</strong> {value[1]}
+                                    </div>
+                                );
+                            }
+                            else {
+                                return (
+                                    <div key={key} className="mb-3">
+                                        <strong>{key}:</strong> Data tidak tersedia
+                                    </div>
+                                );
+                            }
+                        })}
+                    </Card.Body>
+                </Card>
+            ) : (
+                <div>Loading details...</div>
+            )}
+        </Container>
     )
 }
 
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,20 +2,15 @@
 
 import React, { useState, useEffect } from 'react';
 import MultiRangeSlider from "multi-range-slider-react";
-import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import { MapContainer, GeoJSON, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { divIcon } from 'leaflet';
-import indonesia from '../data/indonesia.json';
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import './styles.css';
 import { Link } from 'react-router-dom';
 
 const Map = () => {
-    const [selectedOption, setSelectedOption] = useState('geojson');
     const [datas, setDatas] = useState([]);
     const [minYear, setMinYear] = useState(1600);
     const [maxYear, setMaxYear] = useState(1998);
@@ -26,11 +21,11 @@ const Map = () => {
             method: "GET",
             headers: { "Content-Type": "application/json" }
         })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => setDatas(data))
-        .catch((error) => console.error(error))
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => setDatas(data))
+            .catch((error) => console.error(error))
     }, []);
 
     const maxBounds = [
@@ -48,99 +43,53 @@ const Map = () => {
         className: 'thick-dot-icon'
     })
 
-
-    const changeProvinceColor = (event) => {
-        event.target.options.originalColor = event.target.options.fillColor;
-        event.target.setStyle({
-            fillColor: 'green'
-        });
-    }
-
-    const resetProvinceColor = (event) => {
-        event.target.setStyle({
-            fillColor: event.target.options.originalColor
-        });
-    }
-
-    const onEachProvince = (province, layer) => {
-        const provinceName = province.properties.state;
-        layer.bindPopup(provinceName);
-
-        layer.on({
-            mouseover: changeProvinceColor,
-            mouseout: resetProvinceColor
-        })
-    }
-
     return (
         <Container fluid>
             <h1 className='mt-3' style={{ textAlign: "center" }}> Peta Sejarah Indonesia </h1>
-            
-            <Row className='my-3'>
-                <Col md={{ span: 2, offset: 10 }}>
-                    <Form>
-                        <Form.Group controlId="form.select">
-                            <Form.Select
-                            value={selectedOption}
-                            onChange={(e) => setSelectedOption(e.target.value)}
-                            >
-                            <option value="geojson">Berdasarkan Provinsi</option>
-                            <option value="latlong">Berdasarkan Peristiwa</option>
-                            </Form.Select>
-                        </Form.Group>
-                    </Form>
-                </Col>
-            </Row>
 
             <MapContainer
-                style={{ height: "80vh"}} 
-                zoom={5} 
+                style={{ height: "80vh" }}
+                zoom={5}
                 center={[-2.548926, 118.0148634]}
                 maxBounds={maxBounds}
                 minZoom={3}
                 className='rounded'
             >
-                <TileLayer 
+                <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                {selectedOption === 'geojson' ? (
-                    <GeoJSON 
-                        data={indonesia}
-                        onEachFeature={onEachProvince}
-                    />
-                ) : (
-                    filteredDatas.map((data, index) => (
-                        <Marker
-                        key={data.iri}
+                {filteredDatas.map((data, index) => (
+                    <Marker
+                        key={`${data.iri}_${data.latitude}_${data.longitude}`}
                         position={[data.latitude, data.longitude]}
                         icon={thickDotDivIcon}
-                        >
-                            <Popup>
-                                <h3>{data.name}</h3>
-                                <Link to={`/detail/${data.iri}`}
+                    >
+                        <Popup>
+                            <h3>{data.name}</h3>
+                            <Link to={`/detail/${data.iri}`}
                                 className='btn btn-info'>
-                                    Lihat detail
-                                </Link>
-                            </Popup>
-                        </Marker>
-                    ))
-                )}
+                                Lihat detail
+                            </Link>
+                        </Popup>
+                    </Marker>
+                ))}
+
             </MapContainer>
 
-            <MultiRangeSlider 
-            min={1600}
-            max={1998}
-            step={1}
-            minValue={minYear}
-            maxValue={maxYear}
-            onInput={(e) => {
-                setMinYear(e.minValue);
-                setMaxYear(e.maxValue);
-            }}
-            barInnerColor='blue'
-            ruler={false}
-            className='mt-4'
+            <MultiRangeSlider
+                min={1600}
+                max={1998}
+                step={1}
+                minValue={minYear}
+                maxValue={maxYear}
+                onInput={(e) => {
+                    setMinYear(e.minValue);
+                    setMaxYear(e.maxValue);
+                }}
+                barInnerColor='blue'
+                ruler={false}
+                className='mt-4'
             />
 
         </Container>
