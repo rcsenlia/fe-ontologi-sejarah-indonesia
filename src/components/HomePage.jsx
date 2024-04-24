8eb0a325a516ba4357bb955af2f326523c41edaf
Michael <michaelbalma47@gmail.com>
2024-04-24 08:10:34 +0700

    change css on timeline

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,6 +10,7 @@ const HomePage = () => {
     const [ dataEvents, setDataEvents] = useState([]);
     const [ dataActors, setDataActors] = useState([]);
     const [ dataPlaces, setDataPlaces] = useState([]);
+    const [ isSmallScreen, setIsSmallScreen] = useState(false);
 
     useEffect(() => {
         const fetchTimeline = async () => {
@@ -31,87 +32,183 @@ const HomePage = () => {
         fetchTimeline();
     }, []);
 
-    return (
-        <div>
-            <LandingPage></LandingPage>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'stretch' }}>
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 768);
+        };
+
+        handleResize(); // Call it once to set the initial state
+
+        window.addEventListener('resize', handleResize); // Listen for resize events
+
+        // Cleanup function to remove the event listener when component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    if (isSmallScreen) {
+        return (
+            <div>
+                <LandingPage></LandingPage>
                 <Container fluid>
-                    <div className='container mx-auto px-4' style={{ height: '100%' }}>
-                        <Card style={{ height: '100%', marginBottom: '1px' }}>
+                    <div className='container mx-auto px-4'>
+                        <Card className="mb-4">
                             <Card.Header>Peristiwa Sejarah</Card.Header>
-                            <Card.Body style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                                {dataEvents.map((data, index) => (
-                                    <div key={data + index} className='my-4 mx-25' style={{ flex: 1 }}>
-                                        <Card style={{ height: '100%' }}>
-                                            <Card.Body>
-                                                <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
-                                                <Card.Text style={{textAlign: "justify"}}>
-                                                    {data.summary}
-                                                </Card.Text>
-                                                <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
-                                                    Lihat Timeline {'>>>'}
-                                                </Link>
-                                            </Card.Body>
-                                        </Card>
-                                    </div>
-                                ))}
+                            <Card.Body>
+                                {dataEvents.map((data, index) => {
+                                    return (
+                                        <div key={data + index} className='my-2 mx-25'>
+                                            <Card>
+                                                <Card.Body>
+                                                    <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
+                                                    <Card.Text>
+                                                    </Card.Text>
+                                                    <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
+                                                        Lihat Timeline {'>>>'}
+                                                    </Link>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    )
+                                })}
                             </Card.Body>
                         </Card>
                     </div>
                 </Container>
                 <Container fluid>
-                    <div className='container mx-auto px-4' style={{ height: '100%' }}>
-                        <Card style={{ height: '100%', marginBottom: '16px' }}>
+                    <div className='container mx-auto px-4'>
+                        <Card className="mb-4">
                             <Card.Header>Tokoh Sejarah</Card.Header>
-                            <Card.Body style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                                {dataActors.map((data, index) => (
-                                    <div key={data + index} className='my-4 mx-25' style={{ flex: 1 }}>
-                                        <Card style={{ height: '100%' }}>
-                                            <Card.Body>
-                                                <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
-                                                <Card.Text style={{textAlign: "justify"}}>
-                                                    {data.summary}
-                                                </Card.Text>
-                                                <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
-                                                    Lihat Timeline {'>>>'}
-                                                </Link>
-                                            </Card.Body>
-                                        </Card>
-                                    </div>
-                                ))}
+                            <Card.Body>
+                                {dataActors.map((data, index) => {
+                                    return (
+                                        <div key={data + index} className='my-2 mx-25'>
+                                            <Card>
+                                                <Card.Body>
+                                                    <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
+                                                    <Card.Text>
+                                                    </Card.Text>
+                                                    <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
+                                                        Lihat Timeline {'>>>'}
+                                                    </Link>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    )
+                                })}
                             </Card.Body>
                         </Card>
                     </div>
                 </Container>
                 <Container fluid>
-                    <div className='container mx-auto px-4' style={{ height: '100%' }}>
-                        <Card style={{ height: '100%', marginBottom: '16px' }}>
+                    <div className='container mx-auto px-4'>
+                        <Card className="mb-4">
                             <Card.Header>Tempat</Card.Header>
-                            <Card.Body style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                                {dataPlaces.map((data, index) => (
-                                    <div key={data + index} className='my-4 mx-25' style={{ flex: 1 }}>
-                                        <Card style={{ height: '100%' }}>
-                                            <Card.Body>
-                                                <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
-                                                <Card.Text style={{textAlign: "justify"}}>
-                                                    Lokasi : {data.latitude}, {data.longitude} <br/>
-                                                    {data.summary}
-                                                </Card.Text>
-                                                <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
-                                                    Lihat Timeline {'>>>'}
-                                                </Link>
-                                            </Card.Body>
-                                        </Card>
-                                    </div>
-                                ))}
+                            <Card.Body>
+                                {dataPlaces.map((data, index) => {
+                                    return (
+                                        <div key={data + index} className='my-2 mx-25'>
+                                            <Card>
+                                                <Card.Body>
+                                                    <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
+                                                    <Card.Text>
+                                                    </Card.Text>
+                                                    <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
+                                                        Lihat Timeline {'>>>'}
+                                                    </Link>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    )
+                                })}
                             </Card.Body>
                         </Card>
                     </div>
                 </Container>
             </div>
-        </div>
-    );
-
+        );
+    } else {
+        return (
+            <div>
+                <LandingPage></LandingPage>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'stretch' }}>
+                    <Container fluid>
+                        <div className='container mx-auto px-4' style={{ height: '100%' }}>
+                            <Card style={{ height: '100%', marginBottom: '1px' }}>
+                                <Card.Header>Peristiwa Sejarah</Card.Header>
+                                <Card.Body style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                                    {dataEvents.map((data, index) => (
+                                        <div key={data + index} className='my-4 mx-25' style={{ flex: 1 }}>
+                                            <Card style={{ height: '100%' }}>
+                                                <Card.Body>
+                                                    <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
+                                                    <Card.Text style={{textAlign: "justify"}}>
+                                                        {data.summary}
+                                                    </Card.Text>
+                                                    <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
+                                                        Lihat Timeline {'>>>'}
+                                                    </Link>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    ))}
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Container>
+                    <Container fluid>
+                        <div className='container mx-auto px-4' style={{ height: '100%' }}>
+                            <Card style={{ height: '100%', marginBottom: '16px' }}>
+                                <Card.Header>Tokoh Sejarah</Card.Header>
+                                <Card.Body style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                                    {dataActors.map((data, index) => (
+                                        <div key={data + index} className='my-4 mx-25' style={{ flex: 1 }}>
+                                            <Card style={{ height: '100%' }}>
+                                                <Card.Body>
+                                                    <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
+                                                    <Card.Text style={{textAlign: "justify"}}>
+                                                        {data.summary}
+                                                    </Card.Text>
+                                                    <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
+                                                        Lihat Timeline {'>>>'}
+                                                    </Link>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    ))}
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Container>
+                    <Container fluid>
+                        <div className='container mx-auto px-4' style={{ height: '100%' }}>
+                            <Card style={{ height: '100%', marginBottom: '16px' }}>
+                                <Card.Header>Tempat</Card.Header>
+                                <Card.Body style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                                    {dataPlaces.map((data, index) => (
+                                        <div key={data + index} className='my-4 mx-25' style={{ flex: 1 }}>
+                                            <Card style={{ height: '100%' }}>
+                                                <Card.Body>
+                                                    <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{data.name}</Card.Title>
+                                                    <Card.Text style={{textAlign: "justify"}}>
+                                                        {data.summary}
+                                                    </Card.Text>
+                                                    <Link to={`/timeline/${data.name}/${data.typeLabel}`} className='btn btn-info btn-sm mt-2'>
+                                                        Lihat Timeline {'>>>'}
+                                                    </Link>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    ))}
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Container>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default HomePage;
